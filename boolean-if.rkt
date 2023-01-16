8b7117d2445424d0455440f56592da2e3d62597c
Lan Nguyen <hey@nvlan.me>
2023-01-16 10:03:27 +0700

    feat: add boolean, function pratical

---
+++ boolean-if.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname boolean-if) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+; true
+; false
+
+(define WIDTH 100)
+(define HEIGHT 100)
+(> WIDTH HEIGHT)
+(>= WIDTH HEIGHT)
+
+
+;(string=? "foo" "bar")
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 10 "solid" "blue"))
+
+
+
+(if (< (image-width I2)
+       (image-height I2))
+    "tall"
+    "wide")
---
+++ function.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname function) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+
+(define (lan c)
+  (circle 40 "solid" c))
+
+;(lan "yellow")
+
+
+(above (lan "yellow") (lan "green"))
+
+(lan (string-append "re" "d"))
